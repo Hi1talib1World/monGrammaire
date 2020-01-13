be54adee8580e5b0195311030a9eec72a42d34ba
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-13 10:42:10 +0100

    up

--- app/src/main/java/com/example/mongrammaire/NAVDRAWER.java
+++ app/src/main/java/com/example/mongrammaire/NAVDRAWER.java
@@ -1,5 +1,6 @@
 package com.example.mongrammaire;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -68,12 +69,10 @@ public class NAVDRAWER extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(getApplicationContext(),Translatetool.class));
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
