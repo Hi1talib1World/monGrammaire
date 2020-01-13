52c0b1091014612c4412ce5d470ae9a5bef70055
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-13 11:49:08 +0100

    up holder of cards

--- app/src/main/java/com/example/mongrammaire/mycardsHolder.java
+++ app/src/main/java/com/example/mongrammaire/mycardsHolder.java
@@ -1,4 +1,26 @@
 package com.example.mongrammaire;
 
-public class mycardsHolder {
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class mycardsHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    ImageView mImageIv;
+    TextView mTitleTV,mDescrTV;
+    itemClickListener itemClickListener ;
+    public mycardsHolder(@NonNull View itemView) {
+        super(itemView);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
