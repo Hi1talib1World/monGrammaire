e15fccc3faad60581655475db4771abae82c6621
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-16 04:28:12 +0100

    up

--- app/src/main/java/com/example/mongrammaire/DetailsActivity.java
+++ app/src/main/java/com/example/mongrammaire/DetailsActivity.java
@@ -2,12 +2,22 @@ package com.example.mongrammaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    TextView mTitleTv, mDescTv;
+    ImageView iImageTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        mTitleTv = findViewById(R.id.dtxt1);
+        mDescTv = findViewById(R.id.dtxt2);
+        iImageTV = findViewById(R.id.dimg1);
+
     }
 }
