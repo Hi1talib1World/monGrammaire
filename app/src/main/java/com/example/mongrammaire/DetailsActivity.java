d4d898a65db04cf4500e709fc3e7c18ea00d11b5
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-16 04:47:34 +0100

    up

--- app/src/main/java/com/example/mongrammaire/DetailsActivity.java
+++ app/src/main/java/com/example/mongrammaire/DetailsActivity.java
@@ -1,5 +1,7 @@
 package com.example.mongrammaire;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -15,9 +17,15 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        ActionBar actionBar = getSupportActionBar();
+
         mTitleTv = findViewById(R.id.dtxt1);
         mDescTv = findViewById(R.id.dtxt2);
         iImageTV = findViewById(R.id.dimg1);
+
+        Intent intent = getIntent();
+        intent.getStringExtra("mTitleTV");
+        intent.getStringExtra("mDescTV");
 
     }
 }
--- app/src/main/java/com/example/mongrammaire/MyAdapter.java
+++ app/src/main/java/com/example/mongrammaire/MyAdapter.java
@@ -1,12 +1,16 @@
 package com.example.mongrammaire;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<mycardsHolder> {
@@ -27,7 +31,7 @@ public class MyAdapter extends RecyclerView.Adapter<mycardsHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull mycardsHolder mycardsHolder, int i) {
+    public void onBindViewHolder(@NonNull final mycardsHolder mycardsHolder, int i) {
         mycardsHolder.mTitleTV.setText(models.get(i).getTitle());
         mycardsHolder.mDescrTV.setText(models.get(i).getTitle());
         mycardsHolder.mImageIv.setImageResource(models.get(i).getImg());
@@ -35,7 +39,29 @@ public class MyAdapter extends RecyclerView.Adapter<mycardsHolder> {
         mycardsHolder.setItemClickListener(new itemClickListener() {
             @Override
             public void onItemClick(View v, int pos) {
-                
+
+                String title = models.get(pos).getTitle();
+                String descr = models.get(pos).getDescription();
+                BitmapDrawable bitmapDrawable = (BitmapDrawable)mycardsHolder.mImageIv.getDrawable();
+
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
+                byte[] bytes = stream.toByteArray();
+
+                Intent intent = new Intent(c,DetailsActivity.class);
+                intent.putExtra("mTitleTV",title);
+                intent.putExtra("mDescTV",descr);
+                intent.putExtra("mImgTV",bytes);
+                c.startActivity(intent);
+
+            }
+        });
+
+        mycardsHolder.setItemClickListener(new itemClickListener() {
+            @Override
+            public void onItemClick(View v, int pos) {
+
             }
         });
 
