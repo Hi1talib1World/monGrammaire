002d0f20f131a43e10c6494719b664ead645a2cd
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-18 20:42:13 +0100

    up

--- app/src/main/java/com/example/mongrammaire/CustomFilter.java
+++ app/src/main/java/com/example/mongrammaire/CustomFilter.java
@@ -1,4 +1,47 @@
 package com.example.mongrammaire;
 
-public class CustomFilter {
+import android.widget.Filter;
+
+import java.util.ArrayList;
+
+public class CustomFilter extends Filter {
+    MyAdapter adapter;
+    ArrayList<Model> filterlist;
+
+    public CustomFilter( ArrayList<Model> filterlist, MyAdapter adapter) {
+        this.adapter = adapter;
+        this.filterlist = filterlist;
+    }
+
+    @Override
+    protected FilterResults performFiltering(CharSequence constraint) {
+        FilterResults results = new FilterResults();
+        if (constraint != null && constraint.length()>0){
+
+            constraint constraint.toString().toUpperCase();
+
+            ArrayList<Model> filtredModels = new ArrayList<>();
+
+            for (int i=0;i<filterlist.size(); i++){
+                if(filterlist.get(i).getTitle().toUpperCase().contains(constraint)){
+
+                    filtredModels.add(filterlist.get(i));
+
+                }
+            }
+            results.count = filtredModels.size();
+            results.values = filtredModels;
+        } else{
+            results.count = filterlist.size();
+            results.values = filterlist;
+        }
+        return null;
+    }
+
+    @Override
+    protected void publishResults(CharSequence constraint, FilterResults results) {
+        adapter.models = (ArrayList<Model>) results.values;
+        adapter.notifyDataSetChanged();
+
+    }
 }
