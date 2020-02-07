b8313be02cc3950dd2e75af2f57bdcda39b69488
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-02-08 00:15:03 +0100

    Update ImageModel.java

--- app/src/main/java/com/example/mongrammaire/ImageModel.java
+++ app/src/main/java/com/example/mongrammaire/ImageModel.java
@@ -1,4 +1,12 @@
 package com.example.mongrammaire;
 
-public class ImageModel {
+public class ImageModel {private int image_drawable;
+
+    public int getImage_drawable() {
+        return image_drawable;
+    }
+
+    public void setImage_drawable(int image_drawable) {
+        this.image_drawable = image_drawable;
+    }
 }
