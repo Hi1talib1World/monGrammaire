17440637c83aacb692f335eb91bfe2227d4ccea7
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-02-08 07:06:28 +0100

    Update SlidingImage_Adapter.java

--- app/src/main/java/com/example/mongrammaire/SlidingImage_Adapter.java
+++ app/src/main/java/com/example/mongrammaire/SlidingImage_Adapter.java
@@ -1,4 +1,74 @@
 package com.example.mongrammaire;
 
-public class SlidingImage_Adapter {
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.mongrammaire.ImageModel;
+import com.example.mongrammaire.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Parsania Hardik on 23/04/2016.
+ */
+public class SlidingImage_Adapter extends PagerAdapter {
+
+
+    private ArrayList<ImageModel> imageModelArrayList;
+    private LayoutInflater inflater;
+    private Context context;
+
+
+    public SlidingImage_Adapter(Context context, ArrayList<ImageModel> imageModelArrayList) {
+        this.context = context;
+        this.imageModelArrayList = imageModelArrayList;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return imageModelArrayList.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+        View imageLayout = inflater.inflate(R.layout.slidingimages_layout, view, false);
+
+        assert imageLayout != null;
+        final ImageView imageView = (ImageView) imageLayout
+                .findViewById(R.id.image);
+
+
+        imageView.setImageResource(imageModelArrayList.get(position).getImage_drawable());
+
+        view.addView(imageLayout, 0);
+
+        return imageLayout;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+    }
+
+    @Override
+    public Parcelable saveState() {
+        return null;
+    }
+
+
 }
