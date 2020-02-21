49bc0ccaa4a13d9e018786a39aadebe183d1eab1
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-02-21 18:45:34 +0100

    Update NAVDRAWER.java

--- app/src/main/java/com/example/mongrammaire/NAVDRAWER.java
+++ app/src/main/java/com/example/mongrammaire/NAVDRAWER.java
@@ -59,7 +59,7 @@ public class NAVDRAWER extends AppCompatActivity
         imageModelArrayList = new ArrayList<>();
         imageModelArrayList = populateList();
 
-        init();
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -88,62 +88,6 @@ public class NAVDRAWER extends AppCompatActivity
         return list;
     }
 
-    private void init() {
-
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPager.setAdapter(new SlidingImage_Adapter(NAVDRAWER.this,imageModelArrayList));
-
-        CirclePageIndicator indicator = (CirclePageIndicator)
-                findViewById(R.id.indicator);
-
-        indicator.setViewPager(mPager);
-
-        final float density = getResources().getDisplayMetrics().density;
-
-//Set circle indicator radius
-        indicator.setRadius(5 * density);
-
-        NUM_PAGES =imageModelArrayList.size();
-
-        // Auto start of viewpager
-        final Handler handler = new Handler();
-        final Runnable Update = new Runnable() {
-            public void run() {
-                if (currentPage == NUM_PAGES) {
-                    currentPage = 0;
-                }
-                mPager.setCurrentItem(currentPage++, true);
-            }
-        };
-        Timer swipeTimer = new Timer();
-        swipeTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(Update);
-            }
-        }, 3000, 3000);
-
-        // Pager listener over indicator
-        indicator.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageSelected(int position) {
-                currentPage = position;
-
-            }
-
-            @Override
-            public void onPageScrolled(int pos, float arg1, int arg2) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int pos) {
-
-            }
-        });
-
-    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
