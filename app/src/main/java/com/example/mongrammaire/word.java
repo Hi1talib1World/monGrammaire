813c043dae2ed70ee11cbbbc454b9b4ecde3b93b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-05 00:02:49 +0100

    Update word.java

--- app/src/main/java/com/example/mongrammaire/word.java
+++ app/src/main/java/com/example/mongrammaire/word.java
@@ -1,8 +1,6 @@
 package com.example.mongrammaire;
 
-import android.content.Context;
 import android.content.res.Configuration;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import com.example.mongrammaire.aboutpage.AboutPage;
 
 import java.util.Calendar;
 
