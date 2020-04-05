b42610264e17077df804a4e8a0e40d63fd7bc5e5
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-05 05:16:28 +0100

    Update Adapter.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/Adapter.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/Adapter.java
@@ -1,4 +1,60 @@
 package com.example.mongrammaire.horisontal_cardv;
 
-public class Adapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.mongrammaire.R;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+    private LayoutInflater inflater;
+    private ArrayList<Model> imageModelArrayList;
+
+    public Adapter(Context ctx, ArrayList<Model> imageModelArrayList){
+
+        inflater = LayoutInflater.from(ctx);
+        this.imageModelArrayList = imageModelArrayList;
+    }
+
+    @Override
+    public Adapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.recycler_item, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(Adapter.MyViewHolder holder, int position) {
+
+        holder.iv.setImageResource(imageModelArrayList.get(position).getImage_drawable());
+        holder.time.setText(imageModelArrayList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageModelArrayList.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView time;
+        ImageView iv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            time = (TextView) itemView.findViewById(R.id.tv);
+            iv = (ImageView) itemView.findViewById(R.id.iv);
+        }
+
+    }
 }
