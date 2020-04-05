b34151abc9e087058bfea45c6b3e6389e01e86ac
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-05 02:44:52 +0100

    Update PlayAgain.java

--- app/src/main/java/com/example/mongrammaire/Quiz/PlayAgain.java
+++ app/src/main/java/com/example/mongrammaire/Quiz/PlayAgain.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.mongrammaire.MainGameActivity;
 import com.example.mongrammaire.R;
 
 public class PlayAgain extends Activity {
