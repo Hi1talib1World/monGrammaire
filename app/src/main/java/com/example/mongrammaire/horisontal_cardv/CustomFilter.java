30889c7fb2aa99cd763702e586bd78ae03d35721
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-06 01:29:47 +0100

    Create CustomFilter.java

---
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/CustomFilter.java
@@ -0,0 +1,50 @@
+package com.example.mongrammaire.horisontal_cardv;
+
+import android.widget.Filter;
+
+import com.example.mongrammaire.cards.Model;
+import com.example.mongrammaire.cards.MyAdapter;
+
+import java.util.ArrayList;
+
+public class CustomFilter extends Filter {
+    MyAdapter adapter;
+    ArrayList<Model> filterlist;
+
+    public CustomFilter( ArrayList<Model> filterlist, MyAdapter adapter) {
+        this.adapter = adapter;
+        this.filterlist = filterlist;
+    }
+
+    @Override
+    protected FilterResults performFiltering(CharSequence constraint) {
+        FilterResults results = new FilterResults();
+        if (constraint != null && constraint.length()>0){
+
+            constraint = constraint.toString().toUpperCase();
+
+            ArrayList<Model> filtredModels = new ArrayList<>();
+
+            for (int i=0;i<filterlist.size(); i++){
+                if(filterlist.get(i).getTitle().toUpperCase().contains(constraint)){
+
+                    filtredModels.add(filterlist.get(i));
+
+                }
+            }
+            results.count = filtredModels.size();
+            results.values = filtredModels;
+        } else{
+            results.count = filterlist.size();
+            results.values = filterlist;
+        }
+        return null;
+    }
+
+    @Override
+    protected void publishResults(CharSequence constraint, FilterResults results) {
+        adapter.models = (ArrayList<Model>) results.values;
+        adapter.notifyDataSetChanged();
+
+    }
+}
