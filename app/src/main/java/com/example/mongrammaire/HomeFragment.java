6e26abcdf727f967145de8d986aaf3b71a3ec212
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-16 15:35:43 +0100

    Update HomeFragment.java

--- app/src/main/java/com/example/mongrammaire/HomeFragment.java
+++ app/src/main/java/com/example/mongrammaire/HomeFragment.java
@@ -4,20 +4,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.mongrammaire.horisontal_cardv.Adapter;
+import com.example.mongrammaire.horisontal_cardv.Model;
+
+import java.util.ArrayList;
+
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Model> imageModelArrayList;
+    private Adapter sadapter;
+    public Adapter adapter;
+    private int[] myImageList = new int[]{R.drawable.house, R.drawable.house,R.drawable.house, R.drawable.house,R.drawable.house,R.drawable.house,R.drawable.house};
+    private String[] myImageNameList = new String[]{"Apple","Mango" ,"Strawberry","Pineapple","Orange","Blueberry","Watermelon"};
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
 
 
+        recyclerView = (RecyclerView) v.findViewById(R.id.recycler);
+
+        imageModelArrayList = eatFruits();
+        adapter = new Adapter(this, imageModelArrayList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        return v;
+    }
+    private ArrayList<Model> eatFruits(){
+
+        ArrayList<Model> list = new ArrayList<>();
+
+        for(int i = 0; i < 7; i++){
+            Model fruitModel = new Model();
+            fruitModel.setName(myImageNameList[i]);
+            fruitModel.setImage_drawable(myImageList[i]);
+            list.add(fruitModel);
+        }
+
+        return list;
     }
 
 
