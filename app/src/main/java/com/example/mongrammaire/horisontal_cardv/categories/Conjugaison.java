e3a7d833c334e2026a284700efc782c35563f5d0
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-16 17:48:55 +0100

    Create Conjugaison.java

---
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Conjugaison.java
@@ -0,0 +1,15 @@
+package com.example.mongrammaire.horisontal_cardv.categories;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mongrammaire.R;
+
+public class Conjugaison extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conjugaison);
+    }
+}
