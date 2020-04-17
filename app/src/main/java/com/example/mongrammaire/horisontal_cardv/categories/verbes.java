9a44b7ba4aa7190c87edac810a2b2e13c57e1f20
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-17 14:02:01 +0100

    Update verbes.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/verbes.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/verbes.java
@@ -1,4 +1,29 @@
 package com.example.mongrammaire.horisontal_cardv.categories;
 
 public class verbes {
+
+    /**
+     * Created by ravi on 16/11/17.
+     */
+
+
+        String verbe;
+        String logo;
+        String desc;
+
+        public verbes() {
+        }
+
+        public String getverbe() {
+            return verbe;
+        }
+
+        public String getlogo() {
+            return logo;
+        }
+
+        public String getdesc() {
+            return desc;
+        }
+
 }
