c629654fa6cd582bacf2c438ca1b39ab04029460
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-17 19:04:00 +0100

    Update Conjugaison.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Conjugaison.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Conjugaison.java
@@ -2,14 +2,143 @@ package com.example.mongrammaire.horisontal_cardv.categories;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
 import com.example.mongrammaire.R;
 
-public class Conjugaison extends AppCompatActivity {
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class Conjugaison extends AppCompatActivity  implements verbesAdapter.verbesAdapterListener{
+
+    private static final String TAG = Conjugaison.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private List<verbes> verbesList;
+    private verbesAdapter mAdapter;
+    private SearchView searchView;
+
+    // url to fetch contacts json
+    private static final String URL = "https://api.androidhive.info/json/contacts.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conjugaison);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // toolbar fancy stuff
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.toolbar_title);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        verbesList = new ArrayList<>();
+        mAdapter = new verbesAdapter(this, verbesList, this);
+
+        // white background notification bar
+        whiteNotificationBar(recyclerView);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, DividerItemDecoration.VERTICAL, 36));
+        recyclerView.setAdapter(mAdapter);
+
+        fetchverbes();
+    }
+
+    /**
+     * fetches json by making http calls
+     */
+    private void fetchContacts() {
+        JsonArrayRequest request = new JsonArrayRequest(URL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        if (response == null) {
+                            Toast.makeText(getApplicationContext(), "Couldn't fetch the contacts! Pleas try again.", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+
+                        List<verbes> items = new Gson().fromJson(response.toString(), new TypeToken<List<verbes>>() {
+                        }.getType());
+
+                        // adding contacts to contacts list
+                        verbesList.clear();
+                        verbesList.addAll(items);
+
+                        // refreshing recycler view
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // error in getting json
+                Log.e(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MyApplication.getInstance().addToRequestQueue(request);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed() {
+        // close search view on back button pressed
+        if (!searchView.isIconified()) {
+            searchView.setIconified(true);
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    private void whiteNotificationBar(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int flags = view.getSystemUiVisibility();
+            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            view.setSystemUiVisibility(flags);
+            getWindow().setStatusBarColor(Color.WHITE);
+        }
+    }
+    @Override
+    public void onverbesSelected(verbes verbes) {
+        Toast.makeText(getApplicationContext(), "Selected: " + verbes.getverbe() + ", " + verbes.getdesc(), Toast.LENGTH_LONG).show();
     }
 }
