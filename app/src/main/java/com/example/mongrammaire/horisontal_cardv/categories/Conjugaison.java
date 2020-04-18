6ba48d3f596bca9e7d2720a6a3575f9073472f1c
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-18 09:14:59 +0100

    Update Conjugaison.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Conjugaison.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Conjugaison.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
