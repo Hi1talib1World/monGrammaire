1f75096dc72c3a76e9aae984c3a3b7a80c5f40d8
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-18 21:35:27 +0100

    Update subs.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subs.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subs.java
@@ -1,4 +1,18 @@
 package com.example.mongrammaire.horisontal_cardv.categories.subordonnes;
 
 public class subs {
+    private String subName,desc;
+
+    public subs(String subName, String desc) {
+        this.subName = subName;
+        this.desc = desc;
+    }
+
+    public String getCallerName() {
+        return subName;
+    }
+
+    public String getCallTime() {
+        return desc;
+    }
 }
