494116945679a9d8a9c389d431f803c4bd6d56a0
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-18 21:32:12 +0100

    Update subsAdapter.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subsAdapter.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subsAdapter.java
@@ -1,4 +1,56 @@
 package com.example.mongrammaire.horisontal_cardv.categories.subordonnes;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mongrammaire.R;
+import com.example.mongrammaire.cards.MyAdapter;
+
 public class subsAdapter {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textView;
+        public MyViewHolder(TextView v) {
+            super(v);
+            textView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public subsAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public subsAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
 }
