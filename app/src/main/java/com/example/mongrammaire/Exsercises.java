ea068818857a37c9f35d9d870e0d0a1fa82cb338
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-19 00:17:18 +0100

    Update Exsercises.java

--- app/src/main/java/com/example/mongrammaire/Exsercises.java
+++ app/src/main/java/com/example/mongrammaire/Exsercises.java
@@ -1,5 +1,6 @@
 package com.example.mongrammaire;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
@@ -16,7 +17,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Collections;
 import java.util.List;
@@ -26,10 +29,95 @@ import info.hoang8f.widget.FButton;
 public class Exsercises extends Fragment {
 
 
+    public static final String ARG_ITEM_ID = "favorite_list";
+
+    ListView favoriteList;
+    SharedPreference sharedPreference;
+    List<Product> favorites;
+
+    Activity activity;
+    ProductListAdapter productListAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_exsercises, container, false);
+        View view = inflater.inflate(R.layout.fragment_exsercises, container, false);
+
+        // Get favorite items from SharedPreferences.
+        sharedPreference = new SharedPreference();
+        favorites = sharedPreference.getFavorites(activity);
+
+        if (favorites == null) {
+            showAlert(getResources().getString(R.string.no_favorites_items),
+                    getResources().getString(R.string.no_favorites_msg));
+        } else {
+
+            if (favorites.size() == 0) {
+                showAlert(
+                        getResources().getString(R.string.no_favorites_items),
+                        getResources().getString(R.string.no_favorites_msg));
+            }
+
+            favoriteList = (ListView) view.findViewById(R.id.list_product);
+            if (favorites != null) {
+                productListAdapter = new ProductListAdapter(activity, favorites);
+                favoriteList.setAdapter(productListAdapter);
+
+                favoriteList.setOnItemClickListener(new OnItemClickListener() {
+
+                    public void onItemClick(AdapterView<?> parent, View arg1,
+                                            int position, long arg3) {
+
+                    }
+                });
+
+                favoriteList
+                        .setOnItemLongClickListener(new OnItemLongClickListener() {
+
+                            @Override
+                            public boolean onItemLongClick(
+                                    AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                                ImageView button = (ImageView) view
+                                        .findViewById(R.id.imgbtn_favorite);
+
+                                String tag = button.getTag().toString();
+                                if (tag.equalsIgnoreCase("grey")) {
+                                    sharedPreference.addFavorite(activity,
+                                            favorites.get(position));
+                                    Toast.makeText(
+                                            activity,
+                                            activity.getResources().getString(
+                                                    R.string.add_favr),
+                                            Toast.LENGTH_SHORT).show();
+
+                                    button.setTag("red");
+                                    button.setImageResource(R.drawable.heart_red);
+                                } else {
+                                    sharedPreference.removeFavorite(activity,
+                                            favorites.get(position));
+                                    button.setTag("grey");
+                                    button.setImageResource(R.drawable.heart_grey);
+                                    productListAdapter.remove(favorites
+                                            .get(position));
+                                    Toast.makeText(
+                                            activity,
+                                            activity.getResources().getString(
+                                                    R.string.remove_favr),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                return true;
+                            }
+                        });
+            }
+        }
+        return view;
     }
 
 
