9fcddb9a27cd97aef6f4d6bcc3192819f3e01463
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-18 21:57:06 +0100

    Update subordonnes.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subordonnes.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subordonnes.java
@@ -4,30 +4,66 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 import com.example.mongrammaire.R;
 import com.example.mongrammaire.cards.MyAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class subordonnes extends AppCompatActivity {
 
-    private RecyclerView srecyclerView;
-    private RecyclerView.Adapter sAdapter;
-    private RecyclerView.LayoutManager slayoutManager;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private List<subs> callsList;
+    private subsAdapter callsAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subordonnes);
+        // Set Layout Manager
+        linearLayoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        srecyclerView.setHasFixedSize(true);
+        // Limiting the size
+        recyclerView.setHasFixedSize(true);
 
-        // use a linear layout manager
-        slayoutManager = new LinearLayoutManager(this);
-        srecyclerView.setLayoutManager(slayoutManager);
+        // Initialize list items
+        init();
 
-        // specify an adapter (see also next example)
-        sAdapter = new MyAdapter(myDataset);
-        srecyclerView.setAdapter(sAdapter);
     }
+    private void init(){
+    subsList = new ArrayList<subs>();
+
+    // Initiating Adapter
+    subsAdapter =new subsAdapter(RecyclerViewActivity.this);
+        recyclerView.setAdapter(subsAdapter);
+
+    // Adding some demo data(Call Objects).
+    // You can get them from your data server
+        subsAdapter.add(new subs("John","9:30 AM"));
+        subsAdapter.add(new subs("Rob","9:40 AM"));
+        subsAdapter.add(new subs("Peter","9:45 AM"));
+        subsAdapter.add(new subs("Jack","9:50 AM"));
+        subsAdapter.add(new subs("Bob","9:55 AM"));
+        subsAdapter.add(new subs("Sandy","10:00 AM"));
+        subsAdapter.add(new subs("Kate","10:05 AM"));
+        subsAdapter.add(new subs("Daniel","10:10 AM"));
+        subsAdapter.add(new subs("Roger","10:15 AM"));
+        subsAdapter.add(new subs("Sid","10:20 AM"));
+        subsAdapter.add(new subs("Kora","10:25 AM"));
+        subsAdapter.add(new subs("Nick","10:30 AM"));
+        subsAdapter.add(new subs("Rose","10:35 AM"));
+        subsAdapter.add(new subs("Mia","10:40 AM"));
+        subsAdapter.add(new subs("Scott","10:45 AM"));
+
+    // Set items to adapter
+        callsAdapter.setCallsFeed(callsList);
+        callsAdapter.notifyDataSetChanged();
+}
 }
