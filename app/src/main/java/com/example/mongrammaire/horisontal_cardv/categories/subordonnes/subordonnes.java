b38a2da0d1f9df17639cd87eb28fdef12b653b5a
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-18 23:46:15 +0100

    Update subordonnes.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subordonnes.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subordonnes.java
@@ -37,33 +37,33 @@ public class subordonnes extends AppCompatActivity {
         init();
 
     }
-    private void init(){
-    subsList = new ArrayList<subs>();
+        private void init(){
+        subsList = new ArrayList<subs>();
 
-    // Initiating Adapter
-    subsAdapter =new subsAdapter(RecyclerViewActivity.this);
-        recyclerView.setAdapter(subsAdapter);
+        // Initiating Adapter
+            subsAdapter =new subsAdapter(subordonnes.this);
+            recyclerView.setAdapter(subsAdapter);
 
-    // Adding some demo data(Call Objects).
-    // You can get them from your data server
-        subsAdapter.add(new subs("John","9:30 AM"));
-        subsAdapter.add(new subs("Rob","9:40 AM"));
-        subsAdapter.add(new subs("Peter","9:45 AM"));
-        subsAdapter.add(new subs("Jack","9:50 AM"));
-        subsAdapter.add(new subs("Bob","9:55 AM"));
-        subsAdapter.add(new subs("Sandy","10:00 AM"));
-        subsAdapter.add(new subs("Kate","10:05 AM"));
-        subsAdapter.add(new subs("Daniel","10:10 AM"));
-        subsAdapter.add(new subs("Roger","10:15 AM"));
-        subsAdapter.add(new subs("Sid","10:20 AM"));
-        subsAdapter.add(new subs("Kora","10:25 AM"));
-        subsAdapter.add(new subs("Nick","10:30 AM"));
-        subsAdapter.add(new subs("Rose","10:35 AM"));
-        subsAdapter.add(new subs("Mia","10:40 AM"));
-        subsAdapter.add(new subs("Scott","10:45 AM"));
+        // Adding some demo data(Call Objects).
+        // You can get them from your data server
+            subsList.add(new subs("John","9:30 AM"));
+            subsList.add(new subs("Rob","9:40 AM"));
+            subsList.add(new subs("Peter","9:45 AM"));
+            subsList.add(new subs("Jack","9:50 AM"));
+            subsList.add(new subs("Bob","9:55 AM"));
+            subsList.add(new subs("Sandy","10:00 AM"));
+            subsList.add(new subs("Kate","10:05 AM"));
+            subsList.add(new subs("Daniel","10:10 AM"));
+            subsList.add(new subs("Roger","10:15 AM"));
+            subsList.add(new subs("Sid","10:20 AM"));
+            subsList.add(new subs("Kora","10:25 AM"));
+            subsList.add(new subs("Nick","10:30 AM"));
+            subsList.add(new subs("Rose","10:35 AM"));
+            subsList.add(new subs("Mia","10:40 AM"));
+            subsList.add(new subs("Scott","10:45 AM"));
 
-    // Set items to adapter
-        callsAdapter.setCallsFeed(callsList);
-        callsAdapter.notifyDataSetChanged();
-}
+        // Set items to adapter
+            subsAdapter.setsubsFeed(callsList);
+            subsAdapter.notifyDataSetChanged();
+        }
 }
