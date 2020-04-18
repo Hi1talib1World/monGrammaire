6c4af98e54efda5b32044e1e6f167bfbad6fe1bd
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-18 21:14:05 +0100

    Update subordonnes.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subordonnes.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subordonnes.java
@@ -2,14 +2,32 @@ package com.example.mongrammaire.horisontal_cardv.categories.subordonnes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.mongrammaire.R;
+import com.example.mongrammaire.cards.MyAdapter;
 
 public class subordonnes extends AppCompatActivity {
 
+    private RecyclerView srecyclerView;
+    private RecyclerView.Adapter sAdapter;
+    private RecyclerView.LayoutManager slayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subordonnes);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        srecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        slayoutManager = new LinearLayoutManager(this);
+        srecyclerView.setLayoutManager(slayoutManager);
+
+        // specify an adapter (see also next example)
+        sAdapter = new MyAdapter(myDataset);
+        srecyclerView.setAdapter(sAdapter);
     }
 }
