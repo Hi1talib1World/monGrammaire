73eee4b250c056f3d4529bd3ce3133c7006f602b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-18 21:01:07 +0100

    Create verbeDetails.java

---
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/verbeDetails.java
@@ -0,0 +1,24 @@
+package com.example.mongrammaire.horisontal_cardv.categories;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mongrammaire.R;
+import com.example.mongrammaire.horisontal_cardv.categories.main.SectionsPagerAdapter;
+
+public class verbeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verbe_details);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+    }
+}
