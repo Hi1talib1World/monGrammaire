0fc9f5df1f984189bd4475f49f41cac5d883ac7b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-18 21:57:08 +0100

    Update subsAdapter.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subsAdapter.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subsAdapter.java
@@ -1,44 +1,49 @@
 package com.example.mongrammaire.horisontal_cardv.categories.subordonnes;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.mongrammaire.R;
 import com.example.mongrammaire.cards.MyAdapter;
 
-public class subsAdapter {
+import java.util.ArrayList;
+import java.util.List;
 
-    private List<Call> callsFeed=new ArrayList();
+public class subsAdapter extends RecyclerView.Adapter<subsAdapter.ViewHolder>{
+
+    private List<subs> subsFeed=new ArrayList();
     // Context is not used here but may be required to
     // perform complex operations or call methods from outside
     private Context context;
 
     // Constructor
-    public CallsAdapter(Context context){
+    public subsAdapter(Context context){
         this.context=context;
     }
 
-    public void setCallsFeed(List<Call> callsFeed){
-        this.callsFeed=callsFeed;
+    public void setCallsFeed(List<subs> callsFeed){
+        this.subsFeed=callsFeed;
     }
 
     // Invoked by layout manager to replace the contents of the views
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        Call call = callsFeed.get(position);
-        holder.showCallDetails(call);
+        subs call = subsFeed.get(position);
+        holder.showCallDetails(subs);
     }
 
     @Override
-    public int getItemCount(){return callsFeed.size();}
+    public int getItemCount(){return subsFeed.size();}
 
     // Invoked by layout manager to create new views
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // Attach layout for single cell
-        int layout = R.layout.calls_feed_layout;
+        int layout = R.layout.subs_feed_layout;
         View v = LayoutInflater
                 .from(parent.getContext())
                 .inflate(layout, parent, false);
@@ -53,15 +58,15 @@ public class subsAdapter {
         public ViewHolder(View itemView) {
             super(itemView);
             // Initiate view
-            callerNameTextView=(TextView)itemView.findViewById(R.id.callerName);
-            callTimeTextView=(TextView)itemView.findViewById(R.id.callTime);
+            callerNameTextView=(TextView)itemView.findViewById(R.id.subsName);
+            callTimeTextView=(TextView)itemView.findViewById(R.id.subsTime);
         }
 
 
-        public void showCallDetails(Call call){
+        public void showCallDetails(subs subs){
             // Attach values for each item
-            String callerName   =call.getCallerName();
-            String callTime     =call.getCallTime();
+            String callerName   =subs.getCallerName();
+            String callTime     =subs.getCallTime();
             callerNameTextView.setText(callerName);
             callTimeTextView.setText(callTime);
         }
