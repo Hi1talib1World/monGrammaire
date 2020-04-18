3a42c7ecc3a730be1b3bf85b35df5dd6ba8192c0
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-18 21:01:03 +0100

    Create SectionsPagerAdapter.java

---
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/main/SectionsPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.mongrammaire.horisontal_cardv.categories.main;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.mongrammaire.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+}
