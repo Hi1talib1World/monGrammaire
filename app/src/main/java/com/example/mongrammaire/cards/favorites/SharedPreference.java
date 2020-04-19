28eaa08a202e653f918442463e864573df17ebbc
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-19 02:31:46 +0100

    Create SharedPreference.java

---
+++ app/src/main/java/com/example/mongrammaire/cards/favorites/SharedPreference.java
@@ -0,0 +1,74 @@
+package com.example.mongrammaire.cards.favorites;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.mongrammaire.cards.Model;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SharedPreference {
+    public static final String PREFS_NAME = "PRODUCT_APP";
+    public static final String FAVORITES = "Product_Favorite";
+
+    public SharedPreference() {
+        super();
+    }
+
+    // This four methods are used for maintaining favorites.
+    public void saveFavorites(Context context, List<Model> favorites) {
+        SharedPreferences settings;
+        SharedPreferences.Editor editor;
+
+        settings = context.getSharedPreferences(PREFS_NAME,
+                Context.MODE_PRIVATE);
+        editor = settings.edit();
+
+        Gson gson = new Gson();
+        String jsonFavorites = gson.toJson(favorites);
+
+        editor.putString(FAVORITES, jsonFavorites);
+
+        editor.commit();
+    }
+
+    public void addFavorite(Context context, Model Model) {
+        List<Model> favorites = getFavorites(context);
+        if (favorites == null)
+            favorites = new ArrayList<Model>();
+        favorites.add(Model);
+        saveFavorites(context, favorites);
+    }
+
+    public void removeFavorite(Context context, Model Model) {
+        ArrayList<Model> favorites = getFavorites(context);
+        if (favorites != null) {
+            favorites.remove(Model);
+            saveFavorites(context, favorites);
+        }
+    }
+
+    public ArrayList<Model> getFavorites(Context context) {
+        SharedPreferences settings;
+        List<Model> favorites;
+
+        settings = context.getSharedPreferences(PREFS_NAME,
+                Context.MODE_PRIVATE);
+
+        if (settings.contains(FAVORITES)) {
+            String jsonFavorites = settings.getString(FAVORITES, null);
+            Gson gson = new Gson();
+            Model[] favoriteItems = gson.fromJson(jsonFavorites,
+                    Model[].class);
+
+            favorites = Arrays.asList(favoriteItems);
+            favorites = new ArrayList<Model>(favorites);
+        } else
+            return null;
+
+        return (ArrayList<Model>) favorites;
+    }
+}
