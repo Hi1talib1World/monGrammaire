35ab503bb89805c16bfea40a76c9ea5a2a779af0
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-29 00:25:24 +0000

    Update MotsActivity.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/MotsActivity.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/MotsActivity.java
@@ -2,14 +2,42 @@ package com.example.mongrammaire.horisontal_cardv.categories.Mots;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.example.mongrammaire.R;
 
-public class MotsActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MotsActivity extends AppCompatActivity implements RecyclerViewAdapter.ItemListener {
+
+    RecyclerView recyclerView;
+    ArrayList arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mots);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        arrayList = new ArrayList();
+        arrayList.add(new DataModel("Item 1", R.drawable.battle, "#09A9FF"));
+        arrayList.add(new DataModel("Item 2", R.drawable.beer, "#3E51B1"));
+        arrayList.add(new DataModel("Item 3", R.drawable.ferrari, "#673BB7"));
+        arrayList.add(new DataModel("Item 4", R.drawable.jetpack_joyride, "#4BAA50"));
+        arrayList.add(new DataModel("Item 5", R.drawable.three_d, "#F94336"));
+        arrayList.add(new DataModel("Item 6", R.drawable.terraria, "#0A9B88"));
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, arrayList, this);
+        recyclerView.setAdapter(adapter);
+
+        GridLayoutManager manager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+
+    }
+    @Override
+    public void onItemClick(DataModel item) {
+
+        Toast.makeText(getApplicationContext(), item.text + " is clicked", Toast.LENGTH_SHORT).show();
+
     }
 }
