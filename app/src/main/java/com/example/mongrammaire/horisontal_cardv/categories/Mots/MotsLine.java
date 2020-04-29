b7804d7ce4c189aebb0d439c957622f31d070cac
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-29 15:24:29 +0000

    Update MotsLine.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/MotsLine.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/MotsLine.java
@@ -11,7 +11,7 @@ public class MotsLine extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.linemots);
         StoryView storyView = findViewById(R.id.storyView);
         storyView.resetStoryVisits();
         ArrayList<StoryModel> uris = new ArrayList<>();
