2ad788e637f9de91f1f0becc3a9ada86815081ec
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-01 04:14:18 +0000

    Update MotsLine.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/MotsLine.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/MotsLine.java
@@ -14,6 +14,6 @@ public class MotsLine extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linemots);
-        
+
     }
 }
