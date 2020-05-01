a5889672d3a3071209f71f99340a68b29b4de848
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-01 00:30:10 +0000

    Update MotsLine.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/MotsLine.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/MotsLine.java
@@ -7,18 +7,13 @@ import com.example.mongrammaire.R;
 
 import java.util.ArrayList;
 
+import omari.hamza.storyview.StoryView;
+
 public class MotsLine extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linemots);
-        StoryView storyView = findViewById(R.id.storyView);
-        storyView.resetStoryVisits();
-        ArrayList<StoryModel> uris = new ArrayList<>();
-        uris.add(new StoryModel("https://images.pexels.com/photos/87840/daisy-pollen-flower-nature-87840.jpeg?cs=srgb&dl=plant-flower-macro-87840.jpg&fm=jpg" , "Ankit Kumar","12:00 PM"));
-        uris.add(new StoryModel("https://bornrealist.com/wp-content/uploads/2017/11/Here-Are-Top-10-Cute-Animals-That-Might-Actually-Kill-You.jpg" ,"Panda Man","01:00 AM"));
-        uris.add(new StoryModel("https://www.planwallpaper.com/static/images/animals-4.jpg","Steve","Yesterday"));
-        uris.add(new StoryModel("https://static.boredpanda.com/blog/wp-content/uuuploads/albino-animals/albino-animals-3.jpg","Grambon","10:15 PM"));
-        storyView.setImageUris(uris);
+        
     }
 }
