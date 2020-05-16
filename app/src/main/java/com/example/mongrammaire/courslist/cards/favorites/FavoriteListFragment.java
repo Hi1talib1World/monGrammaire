7827a3571871797e2349557d3063c0b5289147f8
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-16 03:13:28 +0000

    Update FavoriteListFragment.java

--- app/src/main/java/com/example/mongrammaire/courslist/cards/favorites/FavoriteListFragment.java
+++ app/src/main/java/com/example/mongrammaire/courslist/cards/favorites/FavoriteListFragment.java
@@ -39,7 +39,7 @@ public class FavoriteListFragment  extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_product_list, container,
+        View view = inflater.inflate(R.layout.fragment_favorite_list, container,
                 false);
         // Get favorite items from SharedPreferences.
         sharedPreference = new SharedPreference();
