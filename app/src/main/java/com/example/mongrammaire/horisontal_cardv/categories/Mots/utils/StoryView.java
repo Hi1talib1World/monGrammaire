1e4bd4e5e77d9a72164171cec5ebdbfc1c332f00
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-02 15:13:40 +0100

    Update StoryView.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/utils/StoryView.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/utils/StoryView.java
@@ -4,6 +4,7 @@ package com.example.mongrammaire.horisontal_cardv.categories.Mots.utils;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Log;
