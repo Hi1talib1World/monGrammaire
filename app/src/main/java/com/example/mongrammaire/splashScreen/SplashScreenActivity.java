9f9055ba8d50be3a96f85081ca274c47c7ce4f07
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-08-15 01:55:11 +0100

    Update SplashScreenActivity.java

--- app/src/main/java/com/example/mongrammaire/splashScreen/SplashScreenActivity.java
+++ app/src/main/java/com/example/mongrammaire/splashScreen/SplashScreenActivity.java
@@ -1,12 +1,15 @@
 package com.example.mongrammaire.splashScreen;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.mongrammaire.R;
+import com.example.mongrammaire.horisontal_cardv.NAVDRAWER;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,14 @@ public class SplashScreenActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_splash_screen);
 
-
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this, NAVDRAWER.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
