ab714a7ca639fbcfb0369e210e95cf4b10c35652
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 04:31:23 +0000

    Update MyAdapter.java

--- app/src/main/java/com/example/mongrammaire/courslist/cards/favorites/MyAdapter.java
+++ app/src/main/java/com/example/mongrammaire/courslist/cards/favorites/MyAdapter.java
@@ -4,8 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +13,8 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 import com.example.mongrammaire.courslist.cards.mycardsHolder;
 import com.example.mongrammaire.horisontal_cardv.CustomFilter;
