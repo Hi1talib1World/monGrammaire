571598d96bb7f0f10bcc0d0ef4c18b630bf5474b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 04:31:56 +0000

    Update Time_Up.java

--- app/src/main/java/com/example/mongrammaire/Quiz/Time_Up.java
+++ app/src/main/java/com/example/mongrammaire/Quiz/Time_Up.java
@@ -3,9 +3,11 @@ package com.example.mongrammaire.Quiz;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mongrammaire.R;
 
