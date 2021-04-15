9bc83709b4cb03272e2ff13d25b32155b2923ff3
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 04:13:16 +0000

    Update PhrasesActivity.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/PhrasesActivity.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/PhrasesActivity.java
@@ -1,7 +1,8 @@
 package com.example.mongrammaire.horisontal_cardv.categories;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mongrammaire.R;
 
