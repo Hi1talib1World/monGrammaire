94df4b8b06ceaa01fb4ee6269cdfc5fcabf835ce
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 04:23:33 +0000

    Update subordonnes.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subordonnes.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subordonnes.java
@@ -1,9 +1,10 @@
 package com.example.mongrammaire.horisontal_cardv.categories.subordonnes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
