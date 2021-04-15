ccf49f440d1af1704e8cfbc95abe6e74f07db0eb
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 17:13:50 +0000

    Update BlankFragment2.java

--- app/src/main/java/com/example/mongrammaire/BlankFragment2.java
+++ app/src/main/java/com/example/mongrammaire/BlankFragment2.java
@@ -4,14 +4,14 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import org.jsoup.nodes.Element;
 
