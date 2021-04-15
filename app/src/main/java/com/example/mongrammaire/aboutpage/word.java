09433c3bedf57896bbe87784f72921614966e918
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 17:11:48 +0000

    Update word.java

--- app/src/main/java/com/example/mongrammaire/aboutpage/word.java
+++ app/src/main/java/com/example/mongrammaire/aboutpage/word.java
@@ -2,14 +2,14 @@ package com.example.mongrammaire.aboutpage;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.mongrammaire.Element;
 import com.example.mongrammaire.R;
