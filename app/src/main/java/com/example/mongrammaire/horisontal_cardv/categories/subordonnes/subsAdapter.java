fc592ef322ae776016f815adf087752c0a311638
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 04:25:07 +0000

    Update subsAdapter.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subsAdapter.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/subordonnes/subsAdapter.java
@@ -1,11 +1,12 @@
 package com.example.mongrammaire.horisontal_cardv.categories.subordonnes;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mongrammaire.R;
 
