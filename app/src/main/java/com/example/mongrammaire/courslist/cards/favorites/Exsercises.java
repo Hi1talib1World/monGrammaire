816737aa92832e7b442835426388f1382f07d21b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 17:11:29 +0000

    Update Exsercises.java

--- app/src/main/java/com/example/mongrammaire/courslist/cards/favorites/Exsercises.java
+++ app/src/main/java/com/example/mongrammaire/courslist/cards/favorites/Exsercises.java
@@ -4,8 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +13,9 @@ import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.mongrammaire.R;
 
