0ff5156022722a6cabe748492b17dc2cdc035e6a
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 04:21:55 +0000

    Update Adapter.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/Adapter.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/Adapter.java
@@ -1,12 +1,14 @@
 package com.example.mongrammaire.horisontal_cardv;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mongrammaire.HomeFragment;
 import com.example.mongrammaire.R;
