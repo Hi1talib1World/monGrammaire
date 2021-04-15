6af13656b73d77be59165db2d5ca784748a21ebc
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 04:33:05 +0000

    Update MainGameActivity.java

--- app/src/main/java/com/example/mongrammaire/Quiz/MainGameActivity.java
+++ app/src/main/java/com/example/mongrammaire/Quiz/MainGameActivity.java
@@ -6,12 +6,13 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.CountDownTimer;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.mongrammaire.R;
 import com.example.mongrammaire.home;
