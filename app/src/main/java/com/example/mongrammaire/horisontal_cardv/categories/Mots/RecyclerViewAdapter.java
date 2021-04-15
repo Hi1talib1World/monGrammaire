593ae9b5172cdb3b38db57ff675781eac4236942
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 04:06:58 +0000

    Update RecyclerViewAdapter.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/Mots/RecyclerViewAdapter.java
@@ -2,14 +2,15 @@ package com.example.mongrammaire.horisontal_cardv.categories.Mots;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mongrammaire.R;
 
