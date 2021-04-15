7dd53ce7f591e94e120463631bf71ee62af13113
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 17:13:43 +0000

    Update acceuil.java

--- app/src/main/java/com/example/mongrammaire/Alphabets/acceuil.java
+++ app/src/main/java/com/example/mongrammaire/Alphabets/acceuil.java
@@ -2,10 +2,12 @@ package com.example.mongrammaire.Alphabets;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.mongrammaire.R;
 
