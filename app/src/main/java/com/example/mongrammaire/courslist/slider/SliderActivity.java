e5b893b6849f6ada09efd9e71025c7ea84980406
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-15 17:11:10 +0000

    Update SliderActivity.java

--- app/src/main/java/com/example/mongrammaire/courslist/slider/SliderActivity.java
+++ app/src/main/java/com/example/mongrammaire/courslist/slider/SliderActivity.java
@@ -5,9 +5,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +14,10 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.mongrammaire.R;
 import com.example.mongrammaire.courslist.DetailsActivity;
