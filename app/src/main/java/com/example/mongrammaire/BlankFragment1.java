a127a44b2c3295511be4613911361e1a55f79ec2
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-04-23 18:42:43 +0000

    Update BlankFragment1.java

--- app/src/main/java/com/example/mongrammaire/BlankFragment1.java
+++ app/src/main/java/com/example/mongrammaire/BlankFragment1.java
@@ -3,12 +3,12 @@ package com.example.mongrammaire;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 
 public class BlankFragment1 extends Fragment {
