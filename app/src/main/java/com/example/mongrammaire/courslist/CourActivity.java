295e0d19ab9cfb9d7b11ed1ed1bf77ceec99916b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-05-07 05:47:18 +0000

    Create CourActivity.java

---
+++ app/src/main/java/com/example/mongrammaire/courslist/CourActivity.java
@@ -0,0 +1,16 @@
+package com.example.mongrammaire.courslist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mongrammaire.R;
+
+public class CourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cour);
+    }
+}
