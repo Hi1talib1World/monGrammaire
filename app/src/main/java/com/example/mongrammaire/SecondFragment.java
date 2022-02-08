3984b8cc824c2f6f84558d956ce2a9a55aa58887
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-02-08 02:13:55 +0100

    Update SecondFragment.java

--- app/src/main/java/com/example/mongrammaire/SecondFragment.java
+++ app/src/main/java/com/example/mongrammaire/SecondFragment.java
@@ -10,24 +10,10 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SecondFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SecondFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SecondFragment extends Fragment {
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SecondFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static SecondFragment newInstance(String param1, String param2) {
         SecondFragment fragment = new SecondFragment();
@@ -54,11 +40,4 @@ public class SecondFragment extends Fragment {
 
     }
 
-
-
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
