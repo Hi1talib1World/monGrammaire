a06fb370a81093d331ecc23ceaac4d5d7ce1ed9c
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-07-21 04:19:18 +0100

    Create LessonActivity1.java

---
+++ app/src/main/java/com/example/mongrammaire/courslist/LessonActivity1.java
@@ -0,0 +1,15 @@
+package com.example.mongrammaire.courslist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mongrammaire.R;
+
+public class LessonActivity1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cour1);
+    }
+}
