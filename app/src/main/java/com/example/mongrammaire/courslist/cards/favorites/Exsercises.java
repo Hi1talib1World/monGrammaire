5153b129cf8e5468444a2507ec0de0286812f7d6
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-07-21 04:19:05 +0100

    Update Exsercises.java

--- app/src/main/java/com/example/mongrammaire/courslist/cards/favorites/Exsercises.java
+++ app/src/main/java/com/example/mongrammaire/courslist/cards/favorites/Exsercises.java
@@ -24,9 +24,6 @@ import java.util.List;
 
 public class Exsercises extends Fragment {
 
-
-    public static final String ARG_ITEM_ID = "favorite_list";
-
     ListView favoriteList;
     SharedPreference sharedPreference;
     List<Model> favorites;
@@ -48,100 +45,9 @@ public class Exsercises extends Fragment {
         sharedPreference = new SharedPreference();
         favorites = sharedPreference.getFavorites(activity);
 
-        if (favorites == null) {
-            showAlert(getResources().getString(R.string.no_favorites_items),
-                    getResources().getString(R.string.no_favorites_msg));
-        } else {
 
-            if (favorites.size() == 0) {
-                showAlert(
-                        getResources().getString(R.string.no_favorites_items),
-                        getResources().getString(R.string.no_favorites_msg));
-            }
-
-            favoriteList = (ListView) view.findViewById(R.id.list_product);
-            if (favorites != null) {
-                //productListAdapter = new MyAdapter(activity, (ArrayList<Model>) favorites);
-                favoriteList.setAdapter((ListAdapter) productListAdapter);
-
-                favoriteList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                    public void onItemClick(AdapterView<?> parent, View arg1,
-                                            int position, long arg3) {
-
-                    }
-                });
-
-                favoriteList
-                        .setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-
-                            @Override
-                            public boolean onItemLongClick(
-                                    AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                                ImageView button = (ImageView) view
-                                        .findViewById(R.id.imgbtn_favorite);
-
-                                String tag = button.getTag().toString();
-                                if (tag.equalsIgnoreCase("grey")) {
-                                    sharedPreference.addFavorite(activity,
-                                            favorites.get(position));
-                                    Toast.makeText(
-                                            activity,
-                                            activity.getResources().getString(
-                                                    R.string.add_favr),
-                                            Toast.LENGTH_SHORT).show();
-
-                                    button.setTag("red");
-                                    button.setImageResource(R.drawable.red_heart);
-                                } /*else {
-                                    sharedPreference.removeFavorite(activity,
-                                            favorites.get(position));
-                                    button.setTag("grey");
-                                    button.setImageResource(R.drawable.heart_grey);
-                                    productListAdapter.remove(favorites
-                                            .get(position));
-                                    Toast.makeText(
-                                            activity,
-                                            activity.getResources().getString(
-                                                    R.string.remove_favr),
-                                            Toast.LENGTH_SHORT).show();
-                                }*/
-                                return true;
-                            }
-                        });
-            }
-        }
         return view;
     }
 
-    public void showAlert(String title, String message) {
-        if (activity != null && !activity.isFinishing()) {
-            AlertDialog alertDialog = new AlertDialog.Builder(activity)
-                    .create();
-            alertDialog.setTitle(title);
-            alertDialog.setMessage(message);
-            alertDialog.setCancelable(false);
-
-            // setting OK Button
-            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
-                    new DialogInterface.OnClickListener() {
-
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                            // activity.finish();
-                            getFragmentManager().popBackStackImmediate();
-                        }
-                    });
-            alertDialog.show();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        getActivity().setTitle(R.string.favorites);
-        super.onResume();
-    }
 
 }
