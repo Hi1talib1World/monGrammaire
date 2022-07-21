a31be19e89428edf315eac62ea04eb5e8b90d471
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-07-21 04:18:57 +0100

    Update CoursListActivity.java

--- app/src/main/java/com/example/mongrammaire/courslist/CoursListActivity.java
+++ app/src/main/java/com/example/mongrammaire/courslist/CoursListActivity.java
@@ -2,15 +2,74 @@ package com.example.mongrammaire.courslist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.mongrammaire.R;
 
 public class CoursListActivity extends AppCompatActivity {
 
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cour);
+
+        listView = (ListView) findViewById(R.id.list);
+        String[] values = new String[]{"Android1", "Android ListView Item 2",
+                "Simple List View In Android", "List View onClick Event","Android List View OnItemClickListener",
+                "Open New Activity When ListView item Clicked", "List View onClick Source Code", "List View Array Adapter Item Click",
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                if (position == 0) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+
+                /*if (position == 2) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 3) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 4) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 5) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 6) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 7) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity2.class);
+                    startActivityForResult(myIntent, 0);
+                }*/
+            }
+        });
     }
 }
