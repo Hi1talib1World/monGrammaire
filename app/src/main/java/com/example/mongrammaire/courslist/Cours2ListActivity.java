3d91d1058d06a7aa88aac278e88ca477462f2808
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-21 20:50:21 +0100

    Update Cours2ListActivity.java

--- app/src/main/java/com/example/mongrammaire/courslist/Cours2ListActivity.java
+++ app/src/main/java/com/example/mongrammaire/courslist/Cours2ListActivity.java
@@ -1,4 +1,67 @@
 package com.example.mongrammaire.courslist;
 
-public class Cours2ListActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.mongrammaire.R;
+
+public class Cours2ListActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cour2list);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                if (position == 0) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+
+                if (position == 2) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 3) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 4) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 5) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 6) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 7) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+            }
+        });
+    }
+
 }
