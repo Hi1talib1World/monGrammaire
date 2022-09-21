b65e1a4baa8f0ea546f1e6fe8eb7462c0d825410
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-21 19:52:55 +0100

    Update NAVDRAWER.java

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/NAVDRAWER.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/NAVDRAWER.java
@@ -85,9 +85,6 @@ public class NAVDRAWER extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-
-
-
     @Override
     public void onBackPressed() {
         AlertDialog.Builder builder = new AlertDialog.Builder(NAVDRAWER.this);
@@ -111,14 +108,9 @@ public class NAVDRAWER extends AppCompatActivity
 
     }
 
-
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, Translatetool.class);
