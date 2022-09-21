aa8b26b6c3d9edfe65c37c688ca747c005e5bf8a
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-22 00:07:02 +0100

    Update QuestionModel.java

--- app/src/main/java/com/example/mongrammaire/Model/QuestionModel.java
+++ app/src/main/java/com/example/mongrammaire/Model/QuestionModel.java
@@ -1,4 +1,29 @@
 package com.example.mongrammaire.Model;
 
+
 public class QuestionModel {
+
+    String question;
+    String answer;
+
+    public QuestionModel(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
