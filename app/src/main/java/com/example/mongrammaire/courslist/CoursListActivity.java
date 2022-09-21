a982cf8943f25bc3ad8fa6d1903d8bd6f3033225
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-21 20:50:18 +0100

    Update CoursListActivity.java

--- app/src/main/java/com/example/mongrammaire/courslist/CoursListActivity.java
+++ app/src/main/java/com/example/mongrammaire/courslist/CoursListActivity.java
@@ -21,6 +21,47 @@ public class CoursListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cour);
 
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                if (position == 0) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+
+                if (position == 2) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 3) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 4) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 5) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 6) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 7) {
+                    Intent myIntent = new Intent(view.getContext(), LessonActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+            }
+        });
     }
 
 }
