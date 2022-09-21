ce82d2512c364d7a07a508f5e5ebe865cc4abb3a
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-22 00:07:10 +0100

    Update PairModel.java

--- app/src/main/java/com/example/mongrammaire/Model/PairModel.java
+++ app/src/main/java/com/example/mongrammaire/Model/PairModel.java
@@ -1,4 +1,37 @@
 package com.example.mongrammaire.Model;
 
+
 public class PairModel {
+
+    String pair1;
+    String pair2;
+
+    public PairModel(String pair1, String pair2) {
+        this.pair1 = pair1;
+        this.pair2 = pair2;
+    }
+
+    public String getPair1() {
+        return pair1;
+    }
+
+    public void setPair1(String pair1) {
+        this.pair1 = pair1;
+    }
+
+    public String getPair2() {
+        return pair2;
+    }
+
+    public void setPair2(String pair2) {
+        this.pair2 = pair2;
+    }
+
+    @Override
+    public String toString() {
+        return "PairModel{" +
+                "pair1='" + pair1 + '\'' +
+                ", pair2='" + pair2 + '\'' +
+                '}';
+    }
 }
