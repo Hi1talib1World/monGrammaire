92795d16dd9184561b82406a7a5274e1336a3474
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-22 00:07:13 +0100

    Update UserData.java

--- app/src/main/java/com/example/mongrammaire/Model/UserData.java
+++ app/src/main/java/com/example/mongrammaire/Model/UserData.java
@@ -1,4 +1,48 @@
 package com.example.mongrammaire.Model;
 
+
 public class UserData {
+
+    String name;
+    String username;
+    String email;
+
+    public UserData(String name, String username, String email) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
