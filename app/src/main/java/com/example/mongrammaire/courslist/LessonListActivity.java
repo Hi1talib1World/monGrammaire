996b84112931616912f9f946d35af10d6f117b5e
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-24 19:38:18 +0100

    Update LessonListActivity.java

--- app/src/main/java/com/example/mongrammaire/courslist/LessonListActivity.java
+++ app/src/main/java/com/example/mongrammaire/courslist/LessonListActivity.java
@@ -1,12 +1,21 @@
 package com.example.mongrammaire.courslist;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mongrammaire.Data.Repository;
 import com.example.mongrammaire.R;
 
+import org.jetbrains.annotations.NotNull;
+
 public class LessonListActivity extends AppCompatActivity {
+
+    public Repository repository;
+
+    private final Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
