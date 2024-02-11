e32fa05537691c0b83a0a87b67021adade7490c1
Hi1talib1World <hicham.outaleb@Taalim.ma>
2024-02-11 04:01:19 +0100

    up

--- app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/conjugaison/main/PageViewModel.java
+++ app/src/main/java/com/example/mongrammaire/horisontal_cardv/categories/conjugaison/main/PageViewModel.java
@@ -1,27 +1,18 @@
 package com.example.mongrammaire.horisontal_cardv.categories.conjugaison.main;
 
-
-import androidx.arch.core.util.Function;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
+import androidx.arch.core.util.Function;
 
 public class PageViewModel extends ViewModel {
 
     private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
-    private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
-        @Override
-        public String apply(Integer input) {
-            return "Hello world from section: " + input;
-        }
-    });
 
     public void setIndex(int index) {
         mIndex.setValue(index);
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
+
 }
