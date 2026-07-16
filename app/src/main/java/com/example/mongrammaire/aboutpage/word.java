415bf253645ee8b7fe2114082e1ee90ad5456bef
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-07-16 21:07:40 +0100

    feat: update word logic

--- app/src/main/java/com/example/mongrammaire/aboutpage/word.java
+++ app/src/main/java/com/example/mongrammaire/aboutpage/word.java
@@ -63,22 +63,4 @@ public class word extends Fragment {
         return copyRightsElement;
     }
 
-    void simulateDayNight(int currentSetting) {
-        final int DAY = 0;
-        final int NIGHT = 1;
-        final int FOLLOW_SYSTEM = 3;
-
-        int currentNightMode = getResources().getConfiguration().uiMode
-                & Configuration.UI_MODE_NIGHT_MASK;
-        if (currentSetting == DAY && currentNightMode != Configuration.UI_MODE_NIGHT_NO) {
-            AppCompatDelegate.setDefaultNightMode(
-                    AppCompatDelegate.MODE_NIGHT_NO);
-        } else if (currentSetting == NIGHT && currentNightMode != Configuration.UI_MODE_NIGHT_YES) {
-            AppCompatDelegate.setDefaultNightMode(
-                    AppCompatDelegate.MODE_NIGHT_YES);
-        } else if (currentSetting == FOLLOW_SYSTEM) {
-            AppCompatDelegate.setDefaultNightMode(
-                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-        }
-    }
 }
